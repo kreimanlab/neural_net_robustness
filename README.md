b3ac4471993c74d2b04622674b81618ff3dc36b1
Meash <m4rtinsch@gmail.com>
2016-09-20 23:59:22 -0400

    analyze individual tasks only, calculate mean and std of variations, put num_weights figures in own directory

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This project requires Python version >= 3.2.
 
 First, install library requirements by running `pip install -r requirements.txt`.
 
-Then download model weights and datasets by running `python setup.py`. This will:
+Then download model weights and datasets by running `python download_data.py`. This will:
 * download the following weights
   * [alexnet](http://files.heuritech.com/weights/alexnet_weights.h5)
 * download the following datasets
@@ -17,14 +17,25 @@ Use `python run.py` to re-train and evaluate models,
 `python analyze.py` to analyze weights and results.
 
 ### Re-Training
-If the `--weights` argument is not set, the model will be trained on the dataset(s), for instance:
+Run `python run.py` with the `--weights` argument not set to re-train the model on the dataset(s), for instance:
 
     python run.py --model alexnet --dataset VOC2012
 will re-train the model `alexnet` on the dataset `VOC2012`.
 Note that the model will not be trained from scratch but instead start with its associated weights, in the example `weights/alexnet.h5`.
 
 ### Evaluation
-If the `--weights` argument is set, the model will be evaluated on the dataset(s), for instance:
+Run `python run.py` with the `--weights` argument  set to evaluate the model on the dataset(s), for instance:
 
     python run.py --model alexnet --weights alexnet_retrained_on_VOC2012 --dataset VOC2012
 will evaluate the model `alexnet` with the weights `weights/alexnet_retrained_on_VOC2012.h5` on the dataset `VOC2012`.
+
+### Analysis
+Run `python analyze.py <task>` to analyze the results 
+where `<task>` is one of `num_weights`, `weight_diffs`, `performances`.
+
+For the `performances` task, if a given weight file itself does not exist, 
+but variations of it with appended `-num[0-9]+` etc. do exist, 
+then those will be used and the results averaged across these variations.
+For instance, if `--weights alexnet-conv1-draw0.5` is provided 
+and the results for these weights do not exist, the script wil search for 
+`alexnet-conv1-draw0.5-num1.p`, `alexnet-conv1-draw0.5-num2.p` etc. instead.
--- analyze.py
+++ analyze.py
@@ -7,6 +7,7 @@ from collections import OrderedDict
 import numpy as np
 from matplotlib import pyplot
 
+from results import get_results_filepath
 from weights import load_weights, walk, walk_key_chain
 
 
@@ -22,12 +23,13 @@ def __absolute_sum(weights):
     return walk(weights, collect=lambda _, x: np.absolute(x).sum())
 
 
-def __plot_bar(x, y, xticks=None, ylabel=None, save_filepath=None):
-    pyplot.bar(x, y)
+def __plot_bar(x, y, xticks=None, ylabel=None, save_filepath=None, **kwargs):
+    pyplot.bar(x, y, **kwargs)
     pyplot.ylabel(ylabel)
     if xticks is not None:
         pyplot.xticks(x, xticks, rotation=90, ha='left')
     if save_filepath is not None:
+        os.makedirs(os.path.dirname(save_filepath), exist_ok=True)
         pyplot.savefig(save_filepath, bbox_inches='tight')
         pyplot.close()
     else:
@@ -48,12 +50,14 @@ def __plot_weight_metric(weights_metric, figure_filename, ylabel=None):
     __plot_bar(key_indices, values, xticks=keys, ylabel=ylabel, save_filepath=figure_filename)
 
 
-def __plot_num_weights(weights, weights_name):
-    num_weights = walk(weights, collect=lambda _, x: x.size)
-    __plot_weight_metric(num_weights, "figures/weights/%s-num_weights.pdf" % weights_name)
+def __plot_num_weights(weights):
+    for weights_name, weights_values in weights.items():
+        num_weights = walk(weights_values, collect=lambda _, x: x.size)
+        __plot_weight_metric(num_weights, "figures/num_weights/%s.pdf" % weights_name)
 
 
 def __plot_weights_diffs(weights):
+    assert len(weights) >= 2, "Need at least two weights to compare"
     # find pairs
     combinations = map(OrderedDict, itertools.combinations(weights.items(), 2))
     for w1_name, w2_name in combinations:
@@ -62,36 +66,65 @@ def __plot_weights_diffs(weights):
         relative_total_diffs = __absolute_sum(__relativize(weights_diffs))
         # plot
         __plot_weight_metric(relative_total_diffs,
-                             "figures/weights/%s-vs-%s.pdf" % (w1_name, w2_name),
+                             "figures/weight_diffs/%s-vs-%s.pdf" % (w1_name, w2_name),
                              ylabel='Relative absolute diffs')
 
 
-def __plot_performances(weights, datasets):
+def __get_results(dataset, weights_name):
+    results_filepaths = get_results_filepath(dataset, weights_name, search_variations=True)
+    metrics = []
+    for filepath in results_filepaths:
+        with open(filepath, 'rb') as results_file:
+            results = pickle.load(results_file)
+        metrics.append(results['metric'])
+    return np.mean(metrics), np.std(metrics)
+
+
+def __plot_performances_by_weights(weights, datasets):
     for weights_name in weights:
-        metrics = []
+        metric_means = []
+        metric_errs = []
         for dataset in datasets:
-            results_filepath = "results/%s-%s.p" % (dataset, weights_name)
-            with open(results_filepath, 'rb') as results_file:
-                results = pickle.load(results_file)
-            metrics.append(results['metric'])
-        __plot_bar(range(len(metrics)), metrics, save_filepath="figures/performance/%s.pdf" % weights_name)
+            mean, err = __get_results(dataset, weights_name)
+            metric_means.append(mean)
+            metric_errs.append(err)
+        __plot_bar(range(len(metric_means)), metric_means,
+                   save_filepath="figures/performance_by_weights/%s.pdf" % weights_name,
+                   yerr=metric_errs)
+
+
+def __plot_performances_by_datasets(weights, datasets):
+    for dataset in datasets:
+        metric_means = []
+        metric_errs = []
+        for weights_name in weights:
+            mean, err = __get_results(dataset, weights_name)
+            metric_means.append(mean)
+            metric_errs.append(err)
+        __plot_bar(range(len(metric_means)), metric_means,
+                   save_filepath="figures/performance_by_dataset/%s.pdf" % dataset,
+                   yerr=metric_errs)
+
+
+def __plot_performances(weights, datasets):
+    __plot_performances_by_weights(weights, datasets)
+    __plot_performances_by_datasets(weights, datasets)
 
 
 if __name__ == '__main__':
+    # options
+    tasks = {'num_weights': lambda weights, _: __plot_num_weights(weights),
+             'weight_diffs': lambda weights, _: __plot_weights_diffs(weights),
+             'performance': __plot_performances}
     # params - command line
     parser = argparse.ArgumentParser(description='Neural Net Robustness - Analysis')
-    parser.add_argument('--weights', type=str, nargs='+',
-                        default=["alexnet", "alexnet_retrained_on_VOC2012_for_10_epochs"],
+    parser.add_argument('task', type=str, choices=tasks.keys())
+    parser.add_argument('--weights', type=str, nargs='+', default=["alexnet"],
                         help='The set of weights to compare with each other')
-    parser.add_argument('--datasets', type=str, nargs='+', default=['VOC2012'],
-                        help='The datasets to compare the evaluations on',
-                        choices=[d for d in os.listdir("datasets") if os.path.isdir(os.path.join("datasets", d))])
+    parser.add_argument('--datasets', type=str, nargs='+', default=['ILSVRC2012/test'],
+                        help='The datasets to compare the evaluations on')
     args = parser.parse_args()
     print('Running analysis with args', args)
-    assert len(args.weights) >= 2, "Need at least two weights to compare"
-    weights = load_weights(*args.weights, keep_names=True)
-    # compare weights
-    __plot_num_weights(weights[args.weights[0]], args.weights[0])
-    __plot_weights_diffs(weights)
-    # compare performance
-    __plot_performances(weights, args.datasets)
+    weights = args.weights if args.task == 'performance' else load_weights(*args.weights, keep_names=True)
+    task = tasks[args.task]
+    task(weights, args.datasets)
