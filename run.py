bf7af9cef7e1a1a91622545ed6ea0f24b058194b
Meash <m4rtinsch@gmail.com>
2016-09-21 12:18:01 -0400

    Use generator to provide the data for training and evaluation

--- run.py
+++ run.py
@@ -3,61 +3,59 @@ import functools
 import os
 import pickle
 
+import itertools
 import numpy as np
 
 from datasets import get_data
 from net import alexnet, preprocess_images_alexnet
+from results import get_results_filepath
 
 
 def __adapt_to_softmax(x, length):
+    try:
+        len(x)
+    except TypeError:
+        x = [x]
     softmaxs = np.zeros([len(x), length])
     for row, col in enumerate(x):
         softmaxs[row, col] = 1
     return softmaxs
 
 
-def __prepare_data(data_directory, collect_images, output_shape):
-    image_paths, truths = get_data(data_directory)
-    assert len(image_paths) > 0
-    assert len(truths) > 0
-    images = collect_images(image_paths)
-    assert len(truths) == len(image_paths)
-    truths = __adapt_to_softmax(truths, output_shape[1])
-    return images, truths
+def __generator(image_paths, truth_labels, load_image, output_length):
+    assert len(truth_labels) > 0
+    assert len(truth_labels) == len(image_paths)
+    for image_path, truth_label in itertools.cycle(zip(image_paths, truth_labels)):
+        image = load_image(image_path)
+        truth = __adapt_to_softmax(truth_label, output_length)
+        yield image, truth
 
 
-def retrain(model, datasets, prepare_data):
+def retrain(model, datasets_directory, datasets, data_generator, num_epochs=10):
     for retrain_dataset in datasets:
         print("Collect %s" % retrain_dataset)
-        images, truths = prepare_data(data_directory="datasets/%s" % retrain_dataset)
+        image_paths, truth_labels = get_data(os.path.join(datasets_directory, retrain_dataset))
+        generator = data_generator(image_paths, truth_labels)
         print("Retrain on %s" % retrain_dataset)
-        model.fit(images, truths, nb_epoch=args.num_epochs)
-        weights_file = "weights/%s_retrained_on_%s_for_%d_epochs.h5" % (args.model, retrain_dataset, args.num_epochs)
+        # TODO: use best weights based on validation error
+        model.fit_generator(generator, samples_per_epoch=len(truth_labels), nb_epoch=num_epochs)
+        weights_file = "weights/retrain/%s/%s/%depochs.h5" % (args.model, retrain_dataset, args.num_epochs)
         print("Save weights to %s" % weights_file)
+        os.makedirs(os.path.dirname(weights_file), exist_ok=True)
         model.save_weights(weights_file)
 
 
-def __fix_evaluation_metric(metric):
-    """
-    Due to a bug in keras, the metric always has a leading 0.0 which this method removes
-    :param metric: the metric to fix containing two values where the first is a 0
-    :return: the last value of the metric
-    """
-    assert len(metric) == 2
-    return metric[-1]
-
-
-def evaluate(model, weights_names, datasets, prepare_data):
+def evaluate(model, weights_names, datasets_directory, datasets, data_generator):
     # TODO: we need to evaluate this with a SVM to get rid of errors due to re-ordering of labels
     for dataset_name in datasets:
         print("Collect %s" % dataset_name)
-        images, truths = prepare_data(data_directory="datasets/%s" % dataset_name)
+        image_paths, truth_labels = get_data(os.path.join(datasets_directory, dataset_name))
         for weights_name in weights_names:
             print("Evaluating with %s" % weights_name)
             model.load_weights("weights/%s.h5" % weights_name)
-            metric = model.evaluate(images, truths)
-            metric = __fix_evaluation_metric(metric)
-            results_filepath = "results/%s-%s.p" % (dataset_name, weights_name)
+            generator = data_generator(image_paths, truth_labels)
+            metric = model.evaluate_generator(generator, val_samples=len(truth_labels), max_q_size=1000)
+            results_filepath = get_results_filepath(dataset_name, weights_name)
             print("Writing results to %s" % results_filepath)
             with open(results_filepath, 'wb') as results_file:
                 pickle.dump({'metric': metric, 'dataset': dataset_name, 'weights': weights_name}, results_file)
@@ -71,9 +69,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Neural Net Robustness')
     parser.add_argument('--model', type=str, default=next(models.__iter__()),
                         help='The model to run', choices=models.keys())
-    parser.add_argument('--datasets', type=str, nargs='+', default=['VOC2012'],
-                        help='The datasets to either re-train or evaluate the model with',
-                        choices=[d for d in os.listdir("datasets") if os.path.isdir(os.path.join("datasets", d))])
+    parser.add_argument('--datasets_directory', type=str, default='datasets',
+                        help='The directory all datasets are stored in')
+    parser.add_argument('--datasets', type=str, nargs='+', default=['ILSVRC2012/test'],
+                        help='The datasets to either re-train or evaluate the model with')
     parser.add_argument('--weights', type=str, nargs='+', default=None,
                         help='The set of weights to evaluate the model with - re-trains the model if this is not set')
     parser.add_argument('--num_epochs', type=int, default=50,
@@ -81,17 +80,22 @@ if __name__ == '__main__':
     args = parser.parse_args()
     print('Running with args', args)
     weights = args.weights
+    assert weights is None or all(os.path.isfile(os.path.join("weights", weight + ".h5")) for weight in weights)
     datasets = args.datasets
+    assert datasets is not None and \
+           all(os.path.isdir(os.path.join(args.datasets_directory, dataset))
+               and os.path.isfile(os.path.join(args.datasets_directory, dataset, "ground_truths.p"))
+               for dataset in datasets)
     # model
-    model = models[args.model](metrics=['accuracy'])
+    model = models[args.model]()
     output_shape = model.get_output_shape_at(-1)
-    prepare_data = functools.partial(__prepare_data,
-                                     collect_images=image_preprocessors[args.model],
-                                     output_shape=output_shape)
+    generator = functools.partial(__generator,
+                                  load_image=lambda path: image_preprocessors[args.model]([path]),
+                                  output_length=output_shape[1])
     if weights is None:
         print("Retraining")
         model.load_weights("weights/%s.h5" % args.model)
-        retrain(model, datasets, prepare_data)
+        retrain(model, args.datasets_directory, datasets, data_generator=generator, num_epochs=args.num_epochs)
     else:
         print("Evaluating")
-        evaluate(model, weights, datasets, prepare_data)
+        evaluate(model, weights, args.datasets_directory, datasets, data_generator=generator)
